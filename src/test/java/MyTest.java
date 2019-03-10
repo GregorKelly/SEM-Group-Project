3f34768265aa540a68a9854755bd96e01bd20bc5
Alfie Thomasson <(alfiethomasson0@gmail.com)>
2019-03-10 15:15:42 +0000

    Adding test config

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class MyTest
+{
+    @Test
+    void unitTest()
+    {
+        assertEquals(5, 5);
+    }
+
+    @Test
+    void unitTest2()
+    {
+        assertEquals(5, 4);
+    }
+}
+
