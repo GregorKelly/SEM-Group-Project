735357cee157b96e2aa0745c9c039967640c2524
Alfie Thomasson <(alfiethomasson0@gmail.com)>
2019-03-11 11:55:28 +0000

    Testing

---
+++ src/test/java/com/napier/sem/AppTest.java
@@ -0,0 +1,26 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+    }
+
+    @Test
+    void CountriesWorldNotNull()
+    {
+        assertNotNull(app.GetCountries());
+    }
+}
